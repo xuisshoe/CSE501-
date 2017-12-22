7a6312bcf66bd976a32207a0e4549e9443f24fb9
Ron Cytron <roncytron@gmail.com>
2017-12-22 15:19:53 -0600

    method templates now there, with exception throws: this is the move to eliminate errors from code we give as extensions

--- extensions/calculator/Computations.java
+++ extensions/calculator/Computations.java
@@ -3,8 +3,134 @@ package calculator;
 public class Computations {
 
 	/**
-	 * Create the static methods as described on the 
+	 * Complete the static methods as described on the 
 	 * description for this assignment.
 	 */
+	
+
+	/**Double operators**/
+	//Returns the result of adding d1 and d2.
+	public static double addDoubles(double d1, double d2) {
+		throw new UnsupportedOperationException("Complete addDoubles");
+	}
+
+	//Return the result of subtracting d2 from d1.
+	static double subtractDoubles(double d1, double d2) {
+		throw new UnsupportedOperationException("Complete subtractDoubles");
+	}
+
+	//Return the result of dividing d1 by d2.
+	static double divideDoubles(double d1, double d2) {
+		throw new UnsupportedOperationException("Complete divideDoubles");
+	}
+
+	//Return the result of multiplying d1 and d2.
+	static double multiplyDoubles(double d1, double d2) {
+		throw new UnsupportedOperationException("Complete multiplyDoubles");
+	}
+
+
+
+	/**Int operators**/
+
+	//Return the result of adding i1 and i2.
+	static int addInts(int i1, int i2) {
+		throw new UnsupportedOperationException("Complete addInts");
+	}
+
+	//Return the result of subtracting i2 from i1.
+	static int subtractInts(int i1, int i2) {
+		throw new UnsupportedOperationException("Complete subtractInts");
+	}
+
+	//Return the result of dividing i1 by i2.
+	static int divideInts(int i1, int i2) {
+		throw new UnsupportedOperationException("Complete divideInts");
+	}
+
+	//Return the result of multiplying i1 and i2.
+	static int multiplyInts(int i1, int i2) {
+		throw new UnsupportedOperationException("Complete multiplyInts");
+	}
+
+
+
+	/**String operators**/	
+	//	//Return the result of concatenating s2 to s1.
+	static String concatenate(String s1, String s2) {
+		throw new UnsupportedOperationException("Complete concatenate");
+	}
+
+
+
+	/**Boolean operators**/
+	//Return the boolean value that results from an AND operation of b1 and b2
+	static boolean andBoolean(boolean b1, boolean b2) {
+		throw new UnsupportedOperationException("Complete andBoolean");
+	}
+	//Return the boolean value that results from an OR operation of b1 and b2
+	static boolean orBoolean(boolean b1, boolean b2) {
+		throw new UnsupportedOperationException("Complete orBooolean");
+	}
+
+	/**Casting to int**/
+	static int intToInt(int in) {
+		throw new UnsupportedOperationException("Complete intToInt");
+	}
+	static int doubleToInt(double in) {
+		throw new UnsupportedOperationException("Complete doubleToInt");
+	}
+	static int stringToInt(String in) {
+		throw new UnsupportedOperationException("Complete stringToInt");
+	}
+	static int booleanToInt(boolean in) {
+		throw new UnsupportedOperationException("Complete booleanToInt");
+	}
+
+
+	/**Casting to double**/
+	static double intToDouble(int in) {
+		throw new UnsupportedOperationException("Complete intToDouble");
+	}
+	static double doubleToDouble(double in) {
+		throw new UnsupportedOperationException("Complete doubleToDouble");
+	}
+	static double stringToDouble(String in) {
+		throw new UnsupportedOperationException("Complete stringToDouble");
+	}
+	static double booleanToDouble(boolean in) {
+		throw new UnsupportedOperationException("Complete booleanToDouble");
+	}
+
+
+	/**Casting to String**/
+	static String intToString(int in) {
+		throw new UnsupportedOperationException("Complete intToString");
+	}
+	static String doubleToString(double in) {
+		throw new UnsupportedOperationException("Complete doubleToString");
+	}
+	static String stringToString(String in) {
+		throw new UnsupportedOperationException("Complete stringToString");
+	}
+	static String booleanToString(boolean in) {
+		throw new UnsupportedOperationException("Complete booleanToString");
+	}
+
+
+	/**Casting to Boolean**/
+	static boolean intToBoolean(int in) {
+		throw new UnsupportedOperationException("Complete intToBoolean");
+	}
+	static boolean doubleToBoolean(double in) {
+		throw new UnsupportedOperationException("Complete doubleToBoolean");
+	}
+	static boolean stringToBoolean(String in) {
+		throw new UnsupportedOperationException("Complete stringToBoolean");
+	}
+	static boolean booleanToBoolean(boolean in) {
+		throw new UnsupportedOperationException("Complete booleanToBoolean");
+	}
+
 
 }
