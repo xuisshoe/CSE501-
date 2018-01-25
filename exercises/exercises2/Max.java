cc121d1234f008cdddf6111636150fb6b3089879
Xu <Xu@Xu-DeskTop>
2018-01-25 10:17:17 -0600

    exercise max

--- exercises/exercises2/Max.java
+++ exercises/exercises2/Max.java
@@ -1,20 +1,32 @@
 package exercises2;
 
+import cse131.ArgsProcessor;
+
 public class Max {
 
 	public static void main(String[] args) {
 		//
 		// Below, prompt the user for two ints, x and y
 		//
-		
-		
-		
-		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int x = ap.nextInt("Enter the value of x :");
+		int y = ap.nextInt("Enter the value of y :");
 		//
 		// Now use what you have learned about conditional
 		//   execution to print the larger of the two values.
 		//
-		
+		if (x > y){
+			System.out.println("The max number of the two is X.");
+		}
+			
+		else {
+			if (x == y) {
+				System.out.println("Two numbers are equal.");
+			}
+			else {
+				System.out.println("The max number of the two is Y.");
+			}
+		}
 		
 		//
 		// Run this program testing on various inputs for
