0b1e7d479c1f53cf943771ab90cc6d0a1616eb8d
Xu <Xu@Xu-DeskTop>
2018-01-25 10:27:32 -0600

    flipcoin

--- exercises/exercises2/CoinFlip.java
+++ exercises/exercises2/CoinFlip.java
@@ -1,4 +1,5 @@
 package exercises2;
+import cse131.ArgsProcessor;
 
 public class CoinFlip {
 
@@ -14,7 +15,12 @@ public class CoinFlip {
 		//   for any input.  It simply prints
 		// heads or tails
 		//
-
+		if (Math.random()>0.5) {
+			System.out.println("The result is the head of the coin.");
+		}
+		else {
+			System.out.println("The result is the tail of the coin.");
+		}
 	}
 
 }
