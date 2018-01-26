4d4e03fb31cdfc30182ce3206055f178b11b3560
xuhan <xuhan@xus-macbook-air.lan>
2018-01-26 01:01:08 -0600

    finally studio 2 ruin

--- studios/studio2/Ruin.java
+++ studios/studio2/Ruin.java
@@ -1,70 +1,56 @@
 package studio2;
 
 import cse131.ArgsProcessor;
+
 public class Ruin {
 
 	public static void main(String[] args) {
-		
 		ArgsProcessor ap = new ArgsProcessor(args);
-		
-		//startAmount, the amount of money that you start with
-		int startAmount = ap.nextInt("Enter the amount of money that you start with : ");
-		//winChance The probability that you win a gamble
+		// global variables
+		int srt = ap.nextInt("Enter the amount of money that you start with : ");
+		int startAmount = srt;
 		double winChance = ap.nextDouble("Enter the probability that you win a gamble (smaller than 1) : ");
-		//winAmount If you reach this amount of money, then you won
-		int winAmount = ap.nextInt("If you reach this amount of money, then you won : ");
-		// totalPlays The number of times you simulate the problem
-		int totalPlays = ap.nextInt("The number of times you simulate the problem : ");
-		//iteration counter
-		int i = 0;
-		//round counter
+		int winAmount = ap.nextInt("If you reach this amount of money, then you won : ");		
+		int totalPlays = ap.nextInt("The number of times you simulate the problem : ");		
+		int i = 1;
 		int round = 0;
-		//loose times
-		int lost = 0;
+		double win = 0.0;
+		double lossChance = 1 - winChance;
+		double Ruin;
 		
-		// loss chance is 1 - winchance
-		double lossChance = 1.0 - winChance; 
-		
-		
-		//
-		//
-		// calculating probability 
-		
-		while ( i < totalPlays ) {
-			while (startAmount > 0 || startAmount == winAmount) {
-				//win condition
-				double t = Math.random(); // generate random number
-				if (t <= winChance) {
-					startAmount = startAmount + 1;	
+		// experiment 
+		while (i <= totalPlays) {
+			boolean result = false;
+			while (startAmount > 0 && startAmount < winAmount) {
+				double t = Math.random();
+				if (t > winChance) {
+					startAmount = startAmount - 1;
 				}
 				else {
-					startAmount = startAmount - 1; 	
+					startAmount = startAmount + 1;
 				}
-				round++;
-			}
-			i++;
-			if (startAmount == 0) {
-				System.out.println("Simulation "+ i +": " + round + " rounds       loose" );
-				lost = lost + 1;
+				round = round + 1; 	
 			}
 			if (startAmount == winAmount) {
-				System.out.println("Simulation "+ i +": " + round + " rounds       win");	
+			win = win + 1;
+			result = true;	
 			}
-			
+		System.out.println("Simulation " + i + ":   " + round + " rounds \t" + ((result) ? " win":" loose") );
+		i = i + 1;
+		round = 0; //reset round counter
+		startAmount = srt; //reset money
 		}
-	System.out.println("   ");// space
-	System.out.println("Losses : " + lost + "   Simulations: " + i);
-	double actualRuin = lost/i;
-	// calculate real ruin
-	if (lossChance != winChance) {
-		double ruin = ( Math.pow((lossChance/winChance),startAmount) - Math.pow((lossChance/winChance),winAmount) )/( 1-Math.pow((lossChance/winChance),winAmount) );
-		System.out.println("Actual Ruin Rate: " +actualRuin+ "  Expected Ruin rate: " + ruin);
+		// calculate win time versus experiment 
+		System.out.println("Losses :" + (int)(totalPlays - win) + "\t    Simulations: " + totalPlays);
+		//calculate ruin probability 
+		if (lossChance != winChance) {
+			Ruin = (Math.pow(lossChance/winChance, startAmount) - Math.pow(lossChance/winChance, winAmount)) / (1 - Math.pow(lossChance/winChance, winAmount));
+		}
+		else {
+			Ruin = 1 - startAmount / winAmount;
+		}
+		System.out.println("Actual Ruin rate: " + ( (totalPlays - win)/totalPlays ) + "\t Expected Ruin rate: " + Ruin );
 	}
-	else {
-		double ruin = 1 - ( startAmount / winAmount );
-		System.out.println("Actual Ruin Rate: " +actualRuin+ "  Expected Ruin rate: " + ruin);
-	}
-	
-	}
+
 
 }
