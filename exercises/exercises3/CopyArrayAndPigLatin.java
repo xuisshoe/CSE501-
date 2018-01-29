dcf8bea4a4fe6097e4e7fa9767299c0335ba357a
Xu <Xu@Xu-DeskTop>
2018-01-29 17:45:25 -0600

    pig latin

--- exercises/exercises3/CopyArrayAndPigLatin.java
+++ exercises/exercises3/CopyArrayAndPigLatin.java
@@ -10,8 +10,16 @@ public class CopyArrayAndPigLatin {
 		//  So we cannot just do the following (too much to type):
 		//     String[] copy = { names[0], names[1], names[2] };
 		//
+		String [] copy = new String [names.length];
+		String [] pigLatin = new String [copy.length];
 		
-		
+		for (int i = 0; i < names.length; i++ ) {
+			copy[i] = names[i];
+			pigLatin[i] = copy[i] + "ay";
+			System.out.println("The original name is : " + names[i]);
+			System.out.println("The copied name is : " + copy[i] );
+			System.out.println("The pig latin version of the name is : " + copy[i] + "ay");
+		}
 		// 
 		// Follow the instructions on the web page to check
 		// that your copy is correct.  Then cause the copy to store the
