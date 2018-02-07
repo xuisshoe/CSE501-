c623a350372e1d4d7b10d4031496f4ce6f0cf6af
Xu <Xu@Xu-DeskTop.lan>
2018-02-06 22:09:58 -0600

    exercise4 stats

--- exercises/exercises4/Stats.java
+++ exercises/exercises4/Stats.java
@@ -7,7 +7,7 @@ public class Stats {
 
 	public static void main(String[] args) {
 		// prompt the user for the file to be used for this run
-		ArgsProcessor.useStdInput("datafiles/average");
+		ArgsProcessor.useStdInput("datafiles/average/");
 
 		//
 		//  Read in the data from the opened file, computing the
@@ -16,7 +16,28 @@ public class Stats {
 		//
 		//  count is the number of doubles read from the file
 		//  The other statistics should be clear from their names
-		//
+		// global varibal 
+		int count = 0;
+		double sum = 0;
+		double max = Double.MIN_VALUE;
+		double min = Double.MAX_VALUE;
+		//compare loop
+		while (! StdIn.isEmpty()) {
+			double i = StdIn.readDouble();
+			sum = sum + i;
+			count ++;
+			if (i > max) {
+				max = i;
+			}
+			if(i < min) {
+				min = i;
+			}
+		}
+		System.out.println("With " + count + " numbers");
+		System.out.println("The sum is " + sum);
+		System.out.println("The avergae is " + (sum/count));
+		System.out.println("The maximum number is " + max);
+		System.out.println("The maximum number is " + min);
 
 	}
 
