fe68bd07213fa28a534b0c39bf3cfb563a5f43c1
xuhan <xuhan@xus-macbook-pro.dhcp.wustl.edu>
2018-02-07 17:22:30 -0600

    finish pitches compare

--- exercises/exercises4/Pitches.java
+++ exercises/exercises4/Pitches.java
@@ -11,16 +11,31 @@ public class Pitches {
 		//   -1 would be Ab, just below A
 		
 		ArgsProcessor ap = new ArgsProcessor(args);
-		int pitch = ap.nextInt("What pitch ? ");
-		int pitch7 = pitch + 7;
+		int pitch1 = ap.nextInt("What is pitch one ? ");
+		int pitch2 = ap.nextInt("What is pitch two ? ");
+		//int pitch71 = pitch1 + 7;
+		//int pitch72 = pitch2 + 7;
 		
-		double freq = 440.0 * Math.pow(2, pitch/12.0);
-		double freq7 = 440.0 * Math.pow(2, pitch7/12.0);
-		System.out.println("For pitch" + pitch);
-		System.out.println("  The frequency is " + freq);
-		System.out.println("  Up a dinomic fifth " + freq7);
-		double ratio = freq7/freq;
-		System.out.println("The ratio is " + ratio);
+		double freq1 = 440.0 * Math.pow(2, pitch1/12.0);
+		double freq2 = 440.0 * Math.pow(2, pitch2/12.0);
+		
+		//double freq71 = 440.0 * Math.pow(2, pitch71/12.0);
+		//double freq72 = 440.0 * Math.pow(2, pitch72/12.0);
+		
+		System.out.println("For pitch one " + pitch1);
+		System.out.println("  The frequency is " + freq1);
+		//System.out.println("  Up a dinomic fifth " + freq71);
+		//double ratio1 = freq71/freq1;
+		//System.out.println("The ratio one is " + ratio1);
+		
+		System.out.println("For pitch two " + pitch2);
+		System.out.println("  The frequency is " + freq2);
+		//System.out.println("  Up a dinomic fifth " + freq72);
+		//double ratio2 = freq72/freq2;
+		//System.out.println("The ratio one is " + ratio2);
+		
+		double ratio = freq2/freq1;
+		System.out.println("  Ratio is " + ratio);
 		
 
 
