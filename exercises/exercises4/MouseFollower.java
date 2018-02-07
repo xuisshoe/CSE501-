47af837808d10780bbdee882d2d850ed009c2520
xuhan <xuhan@xus-macbook-pro.dhcp.wustl.edu>
2018-02-07 15:48:51 -0600

    delay mouse tracker

--- exercises/exercises4/MouseFollower.java
+++ exercises/exercises4/MouseFollower.java
@@ -8,16 +8,33 @@ import sedgewick.StdDraw;
 public class MouseFollower {
 
 	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int N = ap.nextInt("How much info to store ?");
 		
+		double mouseX[] = new double[N];
+		double mouseY[] = new double[N];
+		int ite = 0;
 		
+		StdDraw.setPenColor(Color.BLUE);
 		while (true) {
 			StdDraw.clear();
 			//
 			// Render one frame of your animation below here
 			//
-
+			double x = StdDraw.mouseX();
+			double y = StdDraw.mouseY();
+			mouseX[ite] = x;
+			mouseY[ite] = y;
+			ite = ite + 1;
 			
-			//  
+			// reset once memoey is out of bond
+			if (ite >= N) {
+				ite = 0;
+			}
+			
+			//int i = ite; // do not think it is necessary
+			StdDraw.filledCircle(mouseX[ite], mouseY[ite], .05);
+  
 			// End of your frame
 			//
 			// Stdraw.show(..) achieves double buffering and
