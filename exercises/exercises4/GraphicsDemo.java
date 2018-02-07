e2b26c77f1fbceaec46a47f4cf57280bf318b805
Xu <Xu@Xu-DeskTop.lan>
2018-02-06 22:57:34 -0600

    exercise Std draw

--- exercises/exercises4/GraphicsDemo.java
+++ exercises/exercises4/GraphicsDemo.java
@@ -8,14 +8,24 @@ public class GraphicsDemo {
 
 	public static void main(String[] args) {
 		// blue point (look carefully, will be very small on your screen)
-		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.point(0.5,0.5);
 		// larger green point
-		
+		StdDraw.setPenColor(Color.GREEN);	
+		StdDraw.setPenRadius(.05);
+		StdDraw.point(.25, .5);
 		// unfilled red triangle
-		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius();
+		StdDraw.line(0, 0, 0, 1);
+		StdDraw.line(0, 1, 0.5 ,0);
+		StdDraw.line(0.5, 0, 0, 0);
 		// yellow circle, filled
-
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.75, 0.75, 0.1);
 		// filled blue rectangle
+		StdDraw.setPenColor( new Color(0,0,155));
+		StdDraw.filledRectangle(0.75, 0.25, 0.03, 0.07);
 		
 
 
