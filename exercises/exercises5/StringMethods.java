9f96ae78cf0c5bc43d03bde2f0dc141381d5b5cb
Xu <Xu@Xu-DeskTop.lan>
2018-02-21 22:21:53 -0600

    exercise 5.2

--- exercises/exercises5/StringMethods.java
+++ exercises/exercises5/StringMethods.java
@@ -13,7 +13,33 @@ public class StringMethods {
 	//
 	
 	public static String selfConcat(String s) {
-		return s + s;
+		//return s + s; rewrite it to use the second method
+		return ncopy(s,2);
+	}
+	// method n copy
+	public static String ncopy(String s, int n) {
+		//String copy = "";
+		//for (int i = 0; i < n; i++) {
+			//copy = copy + s;
+		//}
+		// rewrite the code using join
+		String [] array = new String[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = s;
+		}
+		return join(array,"");
+		
+	}
+	// write the inverse split method
+	public static String join(String[] array, String joiner) {
+		if (array.length == 0) {
+			return "";
+		}
+		String ans = array[0];
+		for(int i = 1; i < array.length; i++ ) {
+			ans = ans + joiner + array[i];
+		}
+		return ans;
 	}
 	
 	public static void main(String[] args) {
@@ -22,6 +48,18 @@ public class StringMethods {
 		// Testing the methods
 		//
 		System.out.println(selfConcat("echo"));
+		System.out.println(ncopy("echo",5));
+		for(int i = 0; i < 5; i++ ) {
+			System.out.println(ncopy(" BAT-MAN ",i));
+		}
+		// test the split function
+		String s = "I like computer science!";
+		String[] words = s.split(" ");
+		for (String w : words) {
+			System.out.println(" word: " + w);
+		}
+		// calling the method join and print it 
+		System.out.println("Back together: " + join(words," "));
 	}
 
 }
