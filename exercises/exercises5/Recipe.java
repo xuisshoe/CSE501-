0ac56d46e9c0fab85a0f6608f820ae86f440490b
Xu <Xu@Xu-DeskTop.lan>
2018-02-21 20:38:33 -0600

    exercise 5.1

--- exercises/exercises5/Recipe.java
+++ exercises/exercises5/Recipe.java
@@ -15,6 +15,21 @@ public class Recipe {
 		// Use those abstractions in comments or code to 
 		//   express the recipe
 		//
+		/* a = mix(water,200cc,yeast,5g);
+		 * heat(better,meltpoint);
+		 * b = mix(butter,vanilla);
+		 * heat(a,100degree);
+		 * delay 5min;
+		 * c = mix(a,b,sugar,flour);
+		 * heat(c,110degree);
+		 * String portion = new String[10];
+		 * for (int i = 0; i < 10; i++){
+		 * 	portion[i] = 0.1*c;
+		 * 	heat(portion[i],350ddegree);
+		 * }
+		 * delay 45min;
+		 * 
+		 */
 	}
 
 }
